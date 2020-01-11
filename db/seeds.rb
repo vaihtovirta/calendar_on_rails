6d93c50dee9fb36b7964e6467f26ea006d0913ba
Emil Shakirov <me+github@emil.sh>
2020-01-11 12:59:07 +0100

    Update to FactoryBot

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-user1 = FactoryGirl.create(:user)
-user2 = FactoryGirl.create(:user)
+user1 = FactoryBot.create(:user)
+user2 = FactoryBot.create(:user)
 
 
 [user1, user2].each do |user|
   10.times do
-    FactoryGirl.create :event,
+    FactoryBot.create :event,
       periodicity: Event.periodicities.keys.sample,
       user: user,
       starts_at: Time.now + rand(1..10).days
