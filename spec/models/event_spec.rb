99d02720db9411f6777d7e48e04ac25e5ced9c13
Emil Shakirov <me+github@emil.sh>
2020-02-17 20:03:29 +0100

    Make Event#user relationship optional

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,7 +6,7 @@ describe Event do
     it { is_expected.to validate_presence_of(:starts_at) }
     it { is_expected.to validate_length_of(:title).is_at_least(5) }
     it { is_expected.to validate_length_of(:title).is_at_most(20) }
-    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:user).required(false) }
   end
 
   context ".by_date" do
